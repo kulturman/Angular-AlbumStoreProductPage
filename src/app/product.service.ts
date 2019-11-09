7c455550be1445fa3503c2bb89ec2216846be2d9
kulturman <arnaudbakyono@gmail.com>
2019-11-09 12:44:42 +0000

    Part 3

---
+++ src/app/product.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Response, Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class ProductService {
+
+  private _albumUrl: string = '../assets/album.json';
+
+  constructor(private _http: Http) { }
+
+  getAlbum(id: number) {
+    return this._http.get(this._albumUrl).map(response => response.json());
+  }
+}
